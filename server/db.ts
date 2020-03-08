ead0bf708fc611c1f0477f778af397858b6a3904
MStokluska <m.stokluska@ymail.com>
2020-03-08 14:04:50 +0000

    fix: removing some unnecessary code

--- server/db.ts
+++ server/db.ts
@@ -37,39 +37,3 @@ async function create() {
                          content varchar(255) not null
     );`);
 }
-
-export const users = [
-  {
-    id: '1',
-    name: 'Michael',
-    password: 'alamo12!',
-  },
-  {
-    id: '2',
-    name: 'Ela',
-    password: 'alamo12!',
-  },
-];
-
-export const chats = [
-  {
-    id: '1',
-    creator: '1',
-    recipent: '2',
-  },
-];
-
-export const message = [
-  {
-    id: '1',
-    mcreator: '1',
-    mchat: '1',
-    content: 'Hi there Ela',
-  },
-  {
-    id: '2',
-    mcreator: '2',
-    mchat: '1',
-    content: 'hi Michael',
-  },
-];

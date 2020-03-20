74e4a45e7cdf56829a856b1c1e2a221fce826bb4
MStokluska <m.stokluska@ymail.com>
2020-03-20 16:28:05 +0000

    feat: adding apollo client and create user mutation

--- Chattie/app/src/main/java/com/mstokluska/chattie/activities/SignUpActivity.kt
+++ Chattie/app/src/main/java/com/mstokluska/chattie/activities/SignUpActivity.kt
@@ -4,25 +4,80 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
 import com.mstokluska.chattie.R
 import com.mstokluska.chattie.main.MainApp
+import com.mstokluska.chattie.models.user.UserModel
+import com.mstokluska.graphql.CreateUserMutation
 import kotlinx.android.synthetic.main.activity_sign_up.*
-import org.jetbrains.anko.toast
+import org.jetbrains.anko.*
+import org.jetbrains.anko.custom.onUiThread
+import java.lang.Math.log
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity(), AnkoLogger {
 
     lateinit var app: MainApp
+    var user = UserModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
         app = application as MainApp
 
-
         btnCreateUser.setOnClickListener() {
-            toast("user created!")
+
+            user.name = user_create_name.text.toString()
+            user.userName = user_create_Nickname.text.toString()
+            user.password = user_create_Password.text.toString()
+
+            if(user.name.isEmpty() or user.userName.isEmpty() or user.password.isEmpty()){
+                toast("Make sure all fields are filled")
+            } else {
+
+                val createUserMutation = CreateUserMutation.builder()
+                    .name(user.name)
+                    .username(user.userName)
+                    .password(user.password)
+                    .build()
+
+
+                app.client
+                    .mutate(createUserMutation)
+                    .enqueue(object : ApolloCall.Callback<CreateUserMutation.Data>() {
+                        override fun onFailure(e: ApolloException) {
+                            e.printStackTrace()
+                        }
+
+                        override fun onResponse(response: Response<CreateUserMutation.Data>) {
+
+                            val resultUserName = response.data()?.createUser()?.username()
+
+                            runOnUiThread {
+
+                                if (resultUserName != null) {
+                                    toast("User Created!")
+                                    finish()
+                                } else {
+                                    toast("Sorry, username already taken")
+                                    user_create_Nickname.setText("")
+                                }
+                            }
+
+                        }
+                    })
+
+
+            }
+
         }
 
+
+
+        
+
+        // menu bar
         toolbarCreateUser.title = title
         setSupportActionBar(toolbarCreateUser)
     }

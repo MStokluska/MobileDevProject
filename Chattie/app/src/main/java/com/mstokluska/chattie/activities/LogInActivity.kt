fd03087480c4009fb1e6c8171c7696bf4a489017
MStokluska <m.stokluska@ymail.com>
2020-03-20 17:53:02 +0000

    feat: login-no pass encryption

--- Chattie/app/src/main/java/com/mstokluska/chattie/activities/LogInActivity.kt
+++ Chattie/app/src/main/java/com/mstokluska/chattie/activities/LogInActivity.kt
@@ -2,23 +2,72 @@ package com.mstokluska.chattie.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
 import com.mstokluska.chattie.R
+import com.mstokluska.chattie.main.MainApp
+import com.mstokluska.chattie.models.user.UserModel
+import com.mstokluska.graphql.CheckUserQuery
+
 
 import kotlinx.android.synthetic.main.log_in_activity.*
-import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.startActivityForResult
-import org.jetbrains.anko.toast
+import org.jetbrains.anko.*
 
 class LogInActivity : AppCompatActivity(), AnkoLogger {
+
+    lateinit var app: MainApp
+    var user = UserModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.log_in_activity)
-
+        app = application as MainApp
 
 
         btnSignIn.setOnClickListener() {
-            startActivityForResult<ChatsActivity>(0)
+            user.userName = userNickname.text.toString()
+            user.password = userPassword.text.toString()
+
+
+            if(user.userName.isEmpty() or user.password.isEmpty()){
+                toast("Username and Password are required")
+            } else {
+
+                val checkUserQuery = CheckUserQuery.builder()
+                    .userName(user.userName)
+                    .password(user.password)
+                    .build()
+
+               app.client
+                   .query(checkUserQuery)
+                   .enqueue(object : ApolloCall.Callback<CheckUserQuery.Data>(){
+
+                       override fun onFailure(e: ApolloException) {
+
+                       }
+
+                       override fun onResponse(response: Response<CheckUserQuery.Data>) {
+
+                           val resultUserName = response.data()?.checkUser()?.username()
+
+                           runOnUiThread {
+                               if (resultUserName != null) {
+                                   toast("Success!")
+                                   startActivityForResult<ChatsActivity>(0)
+                               } else {
+                                   toast("Incorrect username or password")
+                               }
+                           }
+
+                       }
+                   })
+
+
+            }
+
+
+
         }
 
         btnSignUp.setOnClickListener(){
